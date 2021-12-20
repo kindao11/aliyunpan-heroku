654ed51bde60a8ccc504b65ce621c5dda604c0f9
KaiSiMai <51234911+KaiSiMai@users.noreply.github.com>
2021-12-20 19:39:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 
 echo "REFRESH_TOKEN: $REFRESH_TOKEN"
 echo "PORT: $PORT"
-/aliyundrive-webdav --refresh-token $REFRESH_TOKEN --port $PORT --auto-index
+echo "用户: $USER"
+echo "密码: $PASSWORD"
+echo "目录: $ROOT"
+/aliyundrive-webdav --refresh-token $REFRESH_TOKEN --port $PORT -W $PASSWORD -U $USER --root $ROOT
